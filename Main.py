9e85248163717c942ff9c12e5ce6ebeac66890e5
Lennart Krug <Lennart.Krug@web.de>
2019-11-25 11:22:19 +0100

    Camera Selector
    selects  camera and adds priority to external devices

--- Main.py
+++ Main.py
@@ -1,41 +1,54 @@
-import numpy as np
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(1)
+
+def testcam(cap):
+    if cap is None or not cap.isOpened():
+        return False
+    else:
+        return True
 
 
-def rescale_frame(frame,percent=75):
+def init_cam():
+    c = 4
+    cap = cv2.VideoCapture(c)
+    while not testcam(cap):
+        c -= 1
+        cap = cv2.VideoCapture(c)
+    return c
+
+
+def rescale_frame(frame, percent=75):
     scale_percent = 75
-    width = int(frame.shape[1] * scale_percent/100)
-    height = int(frame.shape[0] * scale_percent/100)
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
     dim = (width, height)
-    return cv2.resize(frame,dim,interpolation=cv2.INTER_AREA)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+# ------------------------------------------
+
+
+cap = cv2.VideoCapture(init_cam())
+
 
 while True:
     ret, frame = cap.read()
 
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    frame_blur = cv2.medianBlur(frame_gray,5)
 
-    frame_HSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    frame_blur = cv2.medianBlur(frame_gray, 5)
 
-    white = np.array([127,127,127])
-    black = np.array([255,255,155])
+    frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    white = np.array([127, 127, 127])
+    black = np.array([255, 255, 155])
 
     frame_thres = cv2.inRange(frame_gray, 127, 255)
-   
 
-    
-    
+    cv2.imshow("frame", frame)
+    cv2.imshow("frame_thres ", frame_thres)
 
-
-   
-    cv2.imshow('frame', frame)
-    cv2.imshow('frame_thres ', frame_thres)
-    
-
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord("q"):
         break
 
 
